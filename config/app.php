9afd8dc9d9f7d0da2531032750b907e1b778f372
Aditya <nugroho.aditya@8commerce.com>
2021-04-21 18:59:56 +0700

    first commit

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [ 
+
+    'name' 		=> 'v8', 
+	'debug'		=> env('APP_DEBUG', false),
+    'url' 		=> env('APP_URL', 'http://localhost:8000'),
+
+    'timezone'	=> env('APP_TIMEZONE', 'Asia/Jakarta'),
+
+];
